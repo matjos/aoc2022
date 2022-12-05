f9ed6d686d26b5d8a6ea85dfff8ae0210227177a
Mattias Josefsson <mattias.josefsson@voyado.com>
2022-12-05 09:39:43 +0100

    Dec 5

--- Tests/DEC_5/DEC_5_A_Tests.cs
+++ Tests/DEC_5/DEC_5_A_Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Text.RegularExpressions;
 using FluentAssertions;
 using NUnit.Framework;
 using Tests.Helpers;
@@ -15,6 +16,7 @@ public class DEC_5_A_Tests
         var lines = FileReader.ReadFile(@"DEC_5\input.txt");
         var sum = Sum(lines.ToArray());
         Console.WriteLine(sum);
+        sum.Should().Be("FCVRLMVQP");
     }
     
     [Test]
@@ -23,10 +25,59 @@ public class DEC_5_A_Tests
         var lines = FileReader.ReadFile(@"DEC_5\example.txt");
         var sum = Sum(lines.ToArray());
         Console.WriteLine(sum);
-        //sum.Should().Be(2);
+        sum.Should().Be("CMZ");
     }
-    private static int Sum(string[] lines)
+    private static string Sum(string[] lines)
     {
-        return 0;
+        var result = "";
+        var allStacks = new Dictionary<string, char[]>(); 
+        var isMoving = false;
+        
+        foreach (var line in lines)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                isMoving = true;
+            }
+            else if (isMoving)
+            {
+                var line2 = line.Replace(" ", "-");
+                //var numberOnly = Regex.Replace(line2, "[^0-9.]", "");
+                
+                var numbarray = line2.Split("-");
+
+                //numbarray = numbarray.Where(x => int.TryParse(x) !string.IsNullOrWhiteSpace(x)).ToArray();
+
+                var move = int.Parse(numbarray[1].ToString());
+                var from = numbarray[3].ToString();
+                var to = numbarray[5].ToString();
+
+                var toMove = allStacks[from].Take(move);
+                
+                
+                allStacks[to] = toMove.Reverse().Concat(allStacks[to]).ToArray();
+
+                var temp = allStacks[from].ToList(); 
+                temp.RemoveRange(0, move);
+
+                allStacks[from] = temp.ToArray();
+            }
+            else
+            {
+                var stackId = line.Substring(0, 1);
+                var stackStructure = line.Substring(1);
+                var only_letters = stackStructure.Replace("[","").Replace("]","").Replace(" ","").Replace("\t","").Trim();
+                var stackArray = only_letters.ToCharArray();
+                
+                allStacks.Add(stackId, stackArray); 
+            }
+        }
+
+        foreach (var stack in allStacks)
+        {
+            result += stack.Value.First();
+        }
+        
+        return result;
     }
 }
--- Tests/DEC_5/DEC_5_B_Tests.cs
+++ Tests/DEC_5/DEC_5_B_Tests.cs
@@ -10,24 +10,74 @@ public class DEC_5_B_Tests
 {
 
     [Test]
-    public void Star2()
+    public void Star1()
     {
         var lines = FileReader.ReadFile(@"DEC_5\input.txt");
         var sum = Sum(lines.ToArray());
         Console.WriteLine(sum);
+        sum.Should().Be("RWLWGJGFD");
     }
     
     [Test]
-    public void Star2_Example()
+    public void Star1_Example()
     {
         var lines = FileReader.ReadFile(@"DEC_5\example.txt");
         var sum = Sum(lines.ToArray());
         Console.WriteLine(sum);
-        //sum.Should().Be(4);
+        sum.Should().Be("MCD");
     }
-    private static int Sum(string[] lines)
+    private static string Sum(string[] lines)
     {
-        return 0;
+        var result = "";
+        var allStacks = new Dictionary<string, char[]>(); 
+        var isMoving = false;
+        
+        foreach (var line in lines)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                isMoving = true;
+            }
+            else if (isMoving)
+            {
+                var line2 = line.Replace(" ", "-");
+                //var numberOnly = Regex.Replace(line2, "[^0-9.]", "");
+                
+                var numbarray = line2.Split("-");
+
+                //numbarray = numbarray.Where(x => int.TryParse(x) !string.IsNullOrWhiteSpace(x)).ToArray();
+
+                var move = int.Parse(numbarray[1].ToString());
+                var from = numbarray[3].ToString();
+                var to = numbarray[5].ToString();
+
+                var toMove = allStacks[from].Take(move);
+
+                allStacks[to] = toMove.Concat(allStacks[to]).ToArray();
+
+                var temp = allStacks[from].ToList(); 
+                temp.RemoveRange(0, move);
+
+                allStacks[from] = temp.ToArray();
+            }
+            else
+            {
+                var stackId = line.Substring(0, 1);
+                var stackStructure = line.Substring(1);
+                var only_letters = stackStructure.Replace("[","").Replace("]","").Replace(" ","").Replace("\t","").Trim();
+                var stackArray = only_letters.ToCharArray();
+                
+                allStacks.Add(stackId, stackArray); 
+            }
+        }
+
+        foreach (var stack in allStacks)
+        {
+            result += stack.Value.First();
+        }
+        
+        
+        return result;
     }
 
 }
