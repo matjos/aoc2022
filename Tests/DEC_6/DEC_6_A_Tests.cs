a84d7f8610fd711d2300784a764993cb98356266
Mattias Josefsson <mattias.josefsson@voyado.com>
2022-12-06 07:45:20 +0100

    day 5 plus sceleton 6-7

---
+++ Tests/DEC_6/DEC_6_A_Tests.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Text.RegularExpressions;
+using FluentAssertions;
+using NUnit.Framework;
+using Tests.Helpers;
+
+namespace Tests;
+
+[TestFixture]
+public class DEC_6_A_Tests
+{
+
+    [Test]
+    public void Star1()
+    {
+        var lines = FileReader.ReadFile(@"DEC_6\input.txt");
+        var sum = Sum(lines.ToArray());
+        Console.WriteLine(sum);
+        sum.Should().Be("");
+    }
+    
+    [Test]
+    public void Star1_Example()
+    {
+        var lines = FileReader.ReadFile(@"DEC_6\example.txt");
+        var sum = Sum(lines.ToArray());
+        Console.WriteLine(sum);
+        sum.Should().Be("");
+    }
+    private static string Sum(string[] lines)
+    {
+        return "";
+    }
+}
