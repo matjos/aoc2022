f282973d8fdbd98a1a1732743b0e29623888fe5d
Mattias Josefsson <mattias.josefsson@voyado.com>
2022-12-06 08:13:41 +0100

    Day 8 skeleton

---
+++ Tests/DEC_8/DEC_8_B_Tests.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using FluentAssertions;
+using NUnit.Framework;
+using Tests.Helpers;
+
+namespace Tests;
+
+[TestFixture]
+public class DEC_8_B_Tests
+{
+
+    [Test]
+    public void Star1()
+    {
+        var lines = FileReader.ReadFile(@"DEC_8\input.txt");
+        var sum = Sum(lines.ToArray());
+        Console.WriteLine(sum);
+        sum.Should().Be("");
+    }
+    
+    [Test]
+    public void Star1_Example()
+    {
+        var lines = FileReader.ReadFile(@"DEC_8\example.txt");
+        var sum = Sum(lines.ToArray());
+        Console.WriteLine(sum);
+        sum.Should().Be("");
+    }
+    private static string Sum(string[] lines)
+    {
+        var result = "";
+        return result;
+    }
+
+}
