a66c962a794249960b6c86c0d4469a9682ec8ca8
Mattias Josefsson <mattias.josefsson@voyado.com>
2022-12-08 07:12:07 +0100

    skelleton 9

---
+++ Tests/DEC_9/DEC_9_A_Tests.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+using System.Text.RegularExpressions;
+using FluentAssertions;
+using NUnit.Framework;
+using Tests.Helpers;
+
+namespace Tests;
+
+[TestFixture]
+public class DEC_9_A_Tests : BaseTest
+{
+
+    [Test]
+    public void Star1()
+    {
+        //Arrange
+        timer.StartTimer();
+        var lines = FileReader.ReadFile(@"DEC_8\input.txt");
+        
+        //Act
+        var sum = Sum(lines.ToArray());
+        
+        //Assert
+        Console.WriteLine(sum);
+        sum.Should().Be("");
+        
+        Console.WriteLine(timer.StopTimer());
+    }
+    
+    [Test]
+    public void Star1_Example()
+    {
+        //Arrange
+        timer.StartTimer();
+        var lines = FileReader.ReadFile(@"DEC_8\example.txt");
+        
+        //Act
+        var sum = Sum(lines.ToArray());
+        
+        //Assert
+        Console.WriteLine(sum);
+        sum.Should().Be("");
+        Console.WriteLine(timer.StopTimer());
+    }
+    private static string Sum(string[] lines)
+    {
+        return "";
+    }
+}
